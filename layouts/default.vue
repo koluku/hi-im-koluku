3d2b80d7122a9bc11122c78f98f2cd192e71aa38
koluku <koluku@users.noreply.github.com>
2019-06-25 02:43:25 +0900

    New

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <nuxt />
+  </div>
+</template>
+
+<style lang="scss">
+html {
+  font-family: 'Noto Sans JP', serif;
+  font-size: 16px;
+  line-height: 1.65;
+  letter-spacing: 0.01em;
+  text-align: justify;
+  word-spacing: 1px;
+  background: #00a8ff;
+}
+
+body {
+  margin: 0px;
+  padding: 0px;
+}
+
+.container {
+  box-sizing: border-box;
+  width: 100vw;
+  height: 100vh;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <header id="header">
+      <a href="https://twitter.com/koluku">
+        <img id="logo" src="../static/koluku.png" alt="logo" />
+      </a>
+      <h1 id="title">koluku.com</h1>
+    </header>
+  </div>
+</template>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Patua+One&display=swap');
+
+#header {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+
+#logo {
+  width: 200px;
+  height: 200px;
+  background: #fff;
+  border-radius: 50%;
+  border: solid 5px #fff;
+}
+
+#title {
+  color: #fff;
+  font-size: 3.5rem;
+  font-family: 'Patua One', cursive;
+  margin: 0;
+}
+</style>
